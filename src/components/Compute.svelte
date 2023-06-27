45e0a47a5c76b72055ceab26f72798401de4575c
Jonatan Holmgren <jonatan@jontes.page>
2023-06-27 21:15:00 +0200

    Introduce /Birdbox2023

--- src/components/Compute.svelte
+++ src/components/Compute.svelte
@@ -3,7 +3,6 @@
   import { onMount } from "svelte";
   import { draggable } from "@neodrag/svelte";
   import { slide } from "svelte/transition";
-  import Bird from "./Bird.svelte";
 
   interface Application {
     name: string;
@@ -12,9 +11,6 @@
     [key: string]: Application;
   }
   const applications = writable<Applications>({
-    bird: {
-      name: "Birdwatcher",
-    },
     wallpaper: {
       name: "Wallpaper",
     },
@@ -193,11 +189,7 @@
         <div
           class="bg-neutral-800 text-white p-4 rounded-b-md min-h-[400px] min-w-[400px]"
         >
-          {#if application === "bird"}
-            <p class="text-center">
-              <Bird bird="Blue tit" />
-            </p>
-          {:else if application === "wallpaper"}
+          {#if application === "wallpaper"}
             <div>
               <p>Choose a new wallpaper</p>
               <input
@@ -232,27 +224,6 @@
         class="flex flex-row justify-center gap-x-2 text-white bg-dockbg p-1 rounded-xl shadow-md"
         in:slide={{ duration: 200 }}
       >
-        <button
-          class="p-2 m-1 rounded hover:bg-dockhover"
-          on:click={() => {
-            opened("bird");
-          }}
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 512 512"
-            class="fill-white h-12"
-            ><path
-              d="M160.8 96.5c14 17 31 30.9 49.5 42.2c25.9 15.8 53.7 25.9 77.7 31.6V138.8C265.8 108.5 250 71.5 248.6 28c-.4-11.3-7.5-21.5-18.4-24.4c-7.6-2-15.8-.2-21 5.8c-13.3 15.4-32.7 44.6-48.4 87.2zM320 144v30.6l0 0v1.3l0 0 0 32.1c-60.8-5.1-185-43.8-219.3-157.2C97.4 40 87.9 32 76.6 32c-7.9 0-15.3 3.9-18.8 11C46.8 65.9 32 112.1 32 176c0 116.9 80.1 180.5 118.4 202.8L11.8 416.6C6.7 418 2.6 421.8 .9 426.8s-.8 10.6 2.3 14.8C21.7 466.2 77.3 512 160 512c3.6 0 7.2-1.2 10-3.5L245.6 448H320c88.4 0 160-71.6 160-160V128l29.9-44.9c1.3-2 2.1-4.4 2.1-6.8c0-6.8-5.5-12.3-12.3-12.3H400c-44.2 0-80 35.8-80 80zm80-16a16 16 0 1 1 0 32 16 16 0 1 1 0-32z"
-            /></svg
-          >
-          <p
-            class={"h-2 " +
-              ($openapplications.includes("bird") ? "" : "invisible")}
-          >
-            ·
-          </p></button
-        >
         <button
           class="p-2 m-1 rounded hover:bg-dockhover"
           on:click={() => {
